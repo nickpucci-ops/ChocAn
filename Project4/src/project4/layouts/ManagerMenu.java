85ae6d818480b5e8549b0bf1149aebf0a9710ba2
Kasandra <Kasandra@10.148.142.22>
2024-04-07 13:36:57 -0500

    manager menu update

--- Project4/src/project4/layouts/ManagerMenu.java
+++ Project4/src/project4/layouts/ManagerMenu.java
@@ -1,1 +1,40 @@
+package project4.layouts;
+import java.awt.*;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import project4.Terminal;
+
+public class ManagerMenu extends Menu implements ActionListener {
+	private static final long serialVersionUID = 7059180387276164616L;
+	
+	JPanel mainMenuPanel;
+	JButton runMemberReportBtn;
+	JButton runProviderReportBtn;
+	JButton runManagerReportBtn;
+	JButton runEFTBtn;
+	
+	public ManagerMenu(Terminal terminal) {
+		super(terminal);		
+		this.setTitle("Manager");
+		
+		mainMenuPanel = new JPanel();
+		runMemberReportBtn = new JButton("Run Member Report");
+		runMemberReportBtn.addActionListener(this);
+		mainMenuPanel.add(runMemberReportBtn);
+		runProviderReportBtn = new JButton("Run Provider Report");
+		runProviderReportBtn.addActionListener(this);
+		mainMenuPanel.add(runProviderReportBtn);
+		runManagerReportBtn = new JButton("Run Manager Report");
+		runManagerReportBtn.addActionListener(this);
+		mainMenuPanel.add(runManagerReportBtn);
+		runEFTBtn = new JButton("Run EFT");
+		runEFTBtn.addActionListener(this);
+		mainMenuPanel.add(runEFTBtn);
+		main.add(mainMenuPanel);
+	}
+	
+	
+	
+}
