da6af7fefff3d9758864f703fdaf64b6dee1c9cb
lmillet3804 <135183116+lmillet3804@users.noreply.github.com>
2024-04-09 10:13:29 -0500

    added reference to service codes map

--- Project4/src/project4/report/ProviderDirectory.java
+++ Project4/src/project4/report/ProviderDirectory.java
@@ -1,12 +1,16 @@
 package project4.report;
 
+import java.util.Map.Entry;
+
+import project4.Terminal;
+
 public class ProviderDirectory extends Report {
-	public ProviderDirectory(String filename) {
+	public ProviderDirectory(String filename, Terminal terminal) {
 		super(filename, "Provider Directory");
-		addLineWithTitle("598470", "Dietitian");
-		addLineWithTitle("883948", "Aerobic Exercise");
-		addLineWithTitle("123456", "Chocoholic Counseling");
-		addLineWithTitle("255034", "Consultation");
+		for(Entry<Integer, String> e : terminal.getServiceCodes().entrySet()) {
+			addLineWithTitle(String.valueOf(e.getKey()), e.getValue());
+		}
+		
 		closeAndSave();
 	}
 }
