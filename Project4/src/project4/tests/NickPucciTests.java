99ae668e0abda4986bbfc7ca3cf5bbd5812a25f7
npucci88 <nrpucci@crimson.ua.edu>
2024-04-09 11:23:15 -0500

    Nick Pucci JUnit tests

---
+++ Project4/src/project4/tests/NickPucciTests.java
@@ -0,0 +1,42 @@
+package project4.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import project4.Member;
+import project4.Provider;
+import project4.Terminal;
+
+class NickPucciTests {
+	Terminal terminal;
+	@BeforeEach
+	void setUp() throws Exception {
+		terminal = new Terminal();
+	}
+
+	@Test
+	void testGetMemberNumber() {
+		
+		Member newMember = new Member("test member", 509, "123 Main Street", "Tuscaloosa", "AL", 45567);
+		int num = newMember.getMemberNumber();
+		
+		Assertions.assertTrue(num == 509);
+	}
+	@Test
+	void testGetProviderNumber() {
+		Provider newProvider = new Provider(9, "provider1", "testJohn", 9, "testaddressd", "testcity", "al", 12345);
+		int num = newProvider.getProviderNumber();
+		
+		Assertions.assertTrue(num == 9);
+	}
+	@Test
+	void testGetCity() {
+		Provider newProvider = new Provider(9, "provider1", "testJohn", 9, "testaddressd", "testcity", "al", 12345);
+		String testcity = newProvider.getCity();
+		
+		Assertions.assertTrue(testcity == "testcity");
+	}
+}
