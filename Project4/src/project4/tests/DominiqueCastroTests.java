952bda4849d96d66681ff31e90e1093f75ca3653
dlcastro <dlcastro@crimson.ua.edu>
2024-04-09 14:36:15 -0500

    edit

--- Project4/src/project4/tests/DominiqueCastroTests.java
+++ Project4/src/project4/tests/DominiqueCastroTests.java
@@ -2,40 +2,63 @@ package project4.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.sql.Date;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.Test;
 import project4.*;
 
 class DominiqueCastroTests {
     Terminal terminal;
-
-    @BeforeEach
-    void setUp() throws Exception {
-        terminal = new Terminal();
-    }
+	@BeforeEach
+	void setUp() throws Exception {
+		terminal = new Terminal();
+	}
 
     @Test
     void testMatchingCardNum() {
+    	ArrayList<Employee> employees = terminal.getEmployees();
+		Operator operator = null;
+		for(Employee employee : employees) {
+			if(employee.getEmployeeType() == Employee.OPERATOR) {
+				operator = (Operator)employee;
+				break;
+			}
+		}
+		Assertions.assertTrue(operator != null);
+		operator.addMember(terminal, new Member("Billy Joe", 123, "123 Main Street", "Tuscaloosa", "AL", 35405));
         Card card = new Card("Billy Joe", 123);
-        Member member = new Member("Billy Joe", 123, "123 Main Street", "Tuscaloosa", "AL", 35405);
-        Assertions.assertEquals(card.getMemberNumber(), member.getMemberNumber());
+        Assertions.assertTrue(terminal.getMemberByName("Billy Joe").getMemberNumber() == card.getMemberNumber());
     }
 
     @Test
     void testMatchingCardName() {
-        Card card = new Card("John Doe", 394);
-        Member member = new Member("John Doe", 394, "123 Main Street", "Tuscaloosa", "AL", 35405);
-        Assertions.assertEquals(card.getMemberName(), member.getMemberNumber());
+    	ArrayList<Employee> employees = terminal.getEmployees();
+		Operator operator = null;
+		for(Employee employee : employees) {
+			if(employee.getEmployeeType() == Employee.OPERATOR) {
+				operator = (Operator)employee;
+				break;
+			}
+		}
+		Assertions.assertTrue(operator != null);
+		operator.addMember(terminal, new Member("Billy Joe", 123, "123 Main Street", "Tuscaloosa", "AL", 35405));
+        Card card = new Card("Billy Joe", 123);
+        Assertions.assertTrue(("Billy Joe") == card.getMemberName());
+    
     }
 
     @Test
-    void testService() {
-        Card card = new Card("Billy Joe", 123);
-        Service service = new Service("Consultation", 1, 100, new Date(0), new Date(0), new Member(null, 0, null, null, null, 0), new Provider(0, null, null, 0, null, null, null, 0), "Comments");
-        Assertions.assertEquals(card.getMemberNumber(), service.getMember().getMemberNumber());
-    }
+    void testProviderName() {
+    	Provider provider = new Provider(123, "Provider1", "Joe Joe", 16, "123 Main Street", "Tuscaloosa", "AL", 35405);
+		String Name = provider.getName();
+		Assertions.assertTrue(Name == "Joe Joe");
+	}
 }
